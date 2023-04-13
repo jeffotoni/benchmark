9f87ef34ff2af1a84efd4dd151546e70354b0bc2
jeffotoni <jeff.otoni@gmail.com>
2023-04-13 19:42:10 -0300

    melhoria no elixir  criando headers no response

--- web.client.server/elixir/plug.cowboy/lib/server/router.ex
+++ web.client.server/elixir/plug.cowboy/lib/server/router.ex
@@ -1,7 +1,6 @@
 defmodule Server.Router do
   use Plug.Router
   import Plug.Conn
-  #import Poison
   alias Server.HttpClient
 
   plug :match
@@ -10,15 +9,31 @@ defmodule Server.Router do
   get "/v1/client/get" do
     case HttpClient.fetch_data() do
       {:ok, body} ->
-        send_resp(conn, 200, body)
+        conn
+        |> put_resp_header("Content-Type", "application/json")
+        |> put_resp_header("Engine", "Elixir")
+        |> put_resp_header("Location", "/v1/client/get")
+        |> put_resp_header("Date", DateTime.utc_now() |> DateTime.to_iso8601())
+        |> put_resp_header("Content-Length", Integer.to_string(byte_size(body)))
+        |> send_resp(200, body)
       {:error, error} ->
-        send_resp(conn, 500, error)
+        conn
+        |> put_resp_header("Content-Type", "application/json")
+        |> put_resp_header("Engine", "Elixir")
+        |> put_resp_header("Location", "/v1/client/get")
+        |> put_resp_header("Date", DateTime.utc_now() |> DateTime.to_iso8601())
+        |> put_resp_header("Content-Length", Integer.to_string(byte_size(error)))
+        |> send_resp(500, error)
+        #send_resp(conn, 500, error)
     end
   end
 
-  # suas outras rotas
-
   match _ do
-    send_resp(conn, 404, "Not found")
+    conn
+    |> put_resp_header("Content-Type", "application/json")
+    |> put_resp_header("Engine", "Elixir")
+    |> put_resp_header("Location", "/v1/client/get")
+    |> put_resp_header("Date", DateTime.utc_now() |> DateTime.to_iso8601())
+    |> send_resp(404, "Not found")
   end
 end
